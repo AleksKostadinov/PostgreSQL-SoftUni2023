7a4339ac10e3e9b24c98165673e26ee10647d5b4
AleksKostadinov <kostadinov12@gmail.com>
2023-09-08 16:26:30 +0300

    Exercises

---
+++ 02-PostgreSQL-Basic-CRUD-Lab/01. Select Employee Information.sql
@@ -0,0 +1,17 @@
+SELECT
+    id,
+    CONCAT (first_name, ' ', last_name) AS "Full Name",
+    job_title AS "Job Title"
+FROM employees;
+
+SELECT
+    id,
+    CONCAT_WS (' ', first_name, last_name) AS "Full Name",
+    job_title AS "Job Title"
+FROM employees;
+
+SELECT
+    id,
+    first_name || ' ' || last_name AS "Full Name",
+    job_title AS "Job Title"
+FROM employees;
