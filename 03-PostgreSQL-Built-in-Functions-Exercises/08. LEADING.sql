92356e97a8fec3d58ab4305725d9ae59f9afae5e
AleksKostadinov <kostadinov12@gmail.com>
2023-09-21 00:34:06 +0300

    Exercises

--- 03-PostgreSQL-Built-in-Functions-Exercises/08. LEADING.sql
+++ 03-PostgreSQL-Built-in-Functions-Exercises/08. LEADING.sql
@@ -5,3 +5,7 @@ FROM continents;
 SELECT continent_name,
 TRIM(LEADING FROM continent_name) AS "trim"
 FROM continents;
+
+-- SELECT continent_name,
+-- LTRIM(continent_name) AS "trim"
+-- FROM continents;
--- 03-PostgreSQL-Built-in-Functions-Exercises/18. Arithmetical Operators.sql
+++ 03-PostgreSQL-Built-in-Functions-Exercises/18. Arithmetical Operators.sql
@@ -11,3 +11,11 @@ UPDATE bookings_calculation
     SET
         multiplication = booked_for * 50,
         modulo = booked_for % 50;
+
+-- CREATE TABLE
+--     bookings_calculation AS
+-- SELECT booked_for,
+--     CAST(booked_for * 50 AS NUMERIC) AS multiplication,
+--     CAST(booked_for % 50 AS NUMERIC) AS modulo
+-- FROM bookings
+--     WHERE apartment_id = 93;
