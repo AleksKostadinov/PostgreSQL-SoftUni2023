fbdbe0b22b80768e7ec8eee3ee8df203bc593d0c
AleksKostadinov <kostadinov12@gmail.com>
2023-10-03 00:25:24 +0300

    Exercises

--- 06-PostgreSQL-Subqueries-Joins-and-Indices-Exercises/15. Countries Without Any Mountains.sql
+++ 06-PostgreSQL-Subqueries-Joins-and-Indices-Exercises/15. Countries Without Any Mountains.sql
@@ -9,4 +9,15 @@ WHERE
 	FROM
 		mountains_countries mc
 	);
-    
+
+-- Var. 2
+-- SELECT
+-- 	COUNT(*) AS countries_without_mountains
+-- FROM
+-- 	countries c
+-- LEFT JOIN
+-- 	mountains_countries mc
+-- USING
+-- 	(country_code)
+-- WHERE
+-- 	mc.country_code IS NULL;
